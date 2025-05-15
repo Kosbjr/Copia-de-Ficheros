f25af366cbb896cdabc172cefc3d2f723a08d93b
Héctor <hectorgarcia05.hgg@gmail.com>
2025-05-15 20:55:50 +0200

    Codigo para copiar de un documento de texto a otro

---
+++ copyPaste.java
@@ -0,0 +1,33 @@
+package Examen13_05;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class copyPaste {
+	public static void main(String[]args) throws IOException {
+		
+		BufferedReader inputStream = null;
+		PrintWriter outputStream = null;
+		
+		try {
+			inputStream = new BufferedReader(new FileReader("Tu direccion de archivo"));
+			outputStream = new PrintWriter(new FileWriter("Direccion del archivo que se va a pegar"));
+			
+			String linea;
+			while((linea = inputStream.readLine())!=null) {
+				outputStream.println(linea);
+			}
+			
+			}finally{
+				if(inputStream != null) {
+					inputStream.close();
+				}
+				if(outputStream !=null) {
+					outputStream.close();
+				}
+		}
+	}
+}
